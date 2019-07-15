317e83695795742deb2bab38a3924002799cb000
L_kingming <289437926@qq.com>
2019-07-15 14:55:45 +0800

    c第四节作业

---
+++ 第四节6.21/四.3/Sn/Sn/test.c
@@ -0,0 +1,30 @@
+#if 0
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(){
+
+	int Sn;
+	int a;
+
+	printf("请输入一个数:");
+	scanf("%d", &a);//输入一个数值a
+	Sn = a*10000+2*a*1000+3*a*100+4*a*10+5*a*1;//计算Sn=a+aa+aaa+aaaa+aaaaa;
+	printf("Sn=%d\n", Sn);//打印结果
+	system("pause");
+	return 0;
+}
+#endif
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(){
+	int a = 2;
+	int n = 5;
+	int tmp = 0;
+	int i=0
+
+
+	system("pause");
+	return 0;
+}
