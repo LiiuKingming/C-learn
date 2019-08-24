5cbcdc0661a970e91263b8ede0dc31b886eaf0a3
L_kingming <289437926@qq.com>
2019-08-25 00:04:55 +0800

    2-5

---
+++ C_2-5_common divisor.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int a = 24;
+    int b = 18;
+    int c = 0;  //余数
+    while (a%b != 0){
+        c = a%b;//余数
+        a = b;  //被除数变为下一次的除数
+        b = c;  //余数变为下一次的被除数
+    }
+    printf("%d\n", c);
+    return 0;
+}
