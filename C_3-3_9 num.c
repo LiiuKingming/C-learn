e493615bc73a9d58a0622bc2c7612e00a5198bb4
L_kingming <289437926@qq.com>
2019-09-03 22:28:46 +0800

    3-3

---
+++ C_3-3_9 num.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    int num = 1;
+    for (a = 1; a <= 100; a++){
+        if (a % 10 == 9){
+            num++;
+            printf("%d\n", a);
+        }
+        else if (a / 10 == 9){
+            num++;
+            printf("%d\n", a);
+        }
+    }printf("个数num=%d\n", num);
+    //99当中有两个9
+    return 0;
+}
