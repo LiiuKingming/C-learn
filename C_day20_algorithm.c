2d1302747f8e48418822ee19ac5d1f5bdcc5075c
L_kingming <289437926@qq.com>
2019-09-13 01:33:39 +0800

    day20

---
+++ C_day20_algorithm.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+/*
+int main ()
+{
+    char str[] ="This, a sample string.";
+    char* pch;
+    printf ("Splitting string \"%s\" into tokens:\n",str);
+    pch = strtok (str," ,.-");
+    while (pch != NULL)
+    {
+        printf ("%s\n",pch);
+        pch = strtok (NULL, " ,.-");
+    }
+    return 0;
+}
+ */
+
+
+int main(){
+    char arr[]="     as    adadq     ";
+    char* pch;
+    pch=strtok(arr," ");
+    while( pch != NULL ){
+        printf("%s ",pch);
+        pch=strtok(NULL," ");
+    }
+    return 0;
+}
+
+
